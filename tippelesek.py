350644443de7e424fd32d4fe7d5d633f8276b612
w1ltedg00ze <domosidavid@gmail.com>
2025-11-15 16:39:01 +0100

    repo made

--- tippelesek.py
+++ tippelesek.py
@@ -1,29 +1,37 @@
 import random
-def haromrobotegyszer():
+# def haromrobotegyszer():
+#     szam = random.randint(1,10)
+#     robot1 = random.randint(1,10)
+#     robot2 = random.randint(1, 10)
+#     robot3 = random.randint(1, 10)
+#
+#     eltalalta = ""
+#     eltszam = 0
+#
+#     if robot1 == szam:
+#         eltalalta += "1.robot"
+#         eltszam +=1
+#
+#     if robot2 == szam:
+#         eltalalta+= "2.robot"
+#         eltszam +=1
+#
+#     if robot3 == szam:
+#         eltalalta+= "3.robot"
+#         eltszam +=1
+#
+#
+#     if eltszam == 0:
+#         print("Nem")
+#     elif eltszam <3:
+#         print("eltalálták: ", eltalalta)
+#     else:
+#         print("mind")
+
+
+def egyrobotharomtipp():
     szam = random.randint(1,10)
-    robot1 = random.randint(1,10)
-    robot2 = random.randint(1, 10)
-    robot3 = random.randint(1, 10)
-
-    eltalalta = ""
-    eltszam = 0
-
-    if robot1 == szam:
-        eltalalta += "1.robot"
-        eltszam +=1
-
-    if robot2 == szam:
-        eltalalta+= "2.robot"
-        eltszam +=1
-
-    if robot3 == szam:
-        eltalalta+= "3.robot"
-        eltszam +=1
-
-
-    if eltszam == 0:
-        print("Nem")
-    elif eltszam <3:
-        print("eltalálták: ", eltalalta)
-    else:
-        print("mind")
+    robot = random.randint(1,10)
+    if robot == szam:
+        print("Elsőre")
+    elif
