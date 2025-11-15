9ed2dc2f2d8e1c733f9e6ba7e0d4e997c5bf7e6a
w1ltedg00ze <domosidavid@gmail.com>
2025-11-15 16:17:55 +0100

    siker

--- haromrobotegyszer.py
+++ haromrobotegyszer.py
@@ -4,3 +4,15 @@ def haromrobotegyszer():
     robot1 = random.randint(1,10)
     robot2 = random.randint(1, 10)
     robot3 = random.randint(1, 10)
+
+    eltalalta = ""
+
+    if robot1 == szam:
+        eltalalta += "1.robot"
+
+    if robot2 == szam:
+        eltalalta+= "2.robot"
+
+    if robot3 == szam:
+        eltalalta+= "3.robot"
+    print("eltalálták: ", eltalalta)
